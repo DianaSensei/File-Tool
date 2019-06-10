e0179c60de79f646823cb103d51c36e9d12f0179
DianaSensei <43933486+DianaSensei@users.noreply.github.com>
2019-06-10 11:15:45 +0700

    Class Function
    Add 2 interface.
    Function implement interface

---
+++ File-Tool/IActions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_Tool
+{
+    public interface IActions
+    {
+        IArgs IArg { get; set; }
+
+        string Process(string origin);  
+    }
+    public class Replacer : IActions
+    {
+        public IArgs IArg { get; set; }
+
+        public string Process(string origin)
+        {
+            string result = origin;
+            return result.Replace("Needle","Hammer");
+        }
+    }
+}
---
+++ File-Tool/IArgs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_Tool
+{
+    public interface IArgs
+    {
+    }
+    public class ReplaceArgs : IArgs
+    {
+        string Needle;
+        string Hammer;
+
+    }
+    public class RemoveArgs : IArgs {
+        int StartIndex;
+        int Count;
+    }
+
+    public class NewCaseArgs : IArgs {
+        int Case;
+    }
+    public class FullnameNormalizeArgs : IArgs {
+
+    }
+    public class UniqueNameArgs : IArgs {}
+}
